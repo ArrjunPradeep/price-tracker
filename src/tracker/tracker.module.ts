d00147cab3c7941e7475b90effe60616262ac645
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-10-15 17:49:03 +0530

    schedule-module-pkg-added

--- src/tracker/tracker.module.ts
+++ src/tracker/tracker.module.ts
@@ -3,9 +3,13 @@ import { TrackerService } from './tracker.service';
 import { TrackerController } from './tracker.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Price } from './entities/price.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Price])],
+  imports: [
+    TypeOrmModule.forFeature([Price]),
+    ScheduleModule.forRoot()
+  ],
   controllers: [TrackerController],
   providers: [TrackerService],
 })
