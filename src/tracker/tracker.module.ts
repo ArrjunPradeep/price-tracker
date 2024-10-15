9022ded1bd37d880713d27a5d76c6090f64bf64b
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-10-15 17:45:07 +0530

    module-updated

--- src/tracker/tracker.module.ts
+++ src/tracker/tracker.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TrackerService } from './tracker.service';
 import { TrackerController } from './tracker.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Price } from './entities/price.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Price])],
   controllers: [TrackerController],
   providers: [TrackerService],
 })
