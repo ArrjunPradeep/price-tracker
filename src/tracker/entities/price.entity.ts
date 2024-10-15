92682fca333a1bf663e5e3b98f62f7a336e9b2bf
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-10-15 18:51:53 +0530

    tracker-service-updated

--- src/tracker/entities/price.entity.ts
+++ src/tracker/entities/price.entity.ts
@@ -1,4 +1,5 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, BeforeInsert } from 'typeorm';
+import { format, toZonedTime } from 'date-fns-tz';
 
 @Entity('price')
 export class Price {
@@ -11,6 +12,18 @@ export class Price {
   @Column('decimal', { precision: 10, scale: 2 })
   price: number;
 
+  @Column()
+  formattedTimestamp: string;
+
   @CreateDateColumn()
   timestamp: Date;
+
+  @BeforeInsert()
+  setFormattedTimestamp() {
+    const indiaTimeZone = 'Asia/Kolkata'; // IST timezone
+    const zonedTime = toZonedTime(Date.now(), indiaTimeZone); // Convert to IST
+    
+    // Format as 'DD-MM-YYYY hh:mm:ss A' and save as a string
+    this.formattedTimestamp = format(zonedTime, 'dd-MM-yyyy hh:mm:ss aa', { timeZone: indiaTimeZone });
+  }
 }
--- src/tracker/tracker.service.ts
+++ src/tracker/tracker.service.ts
@@ -4,13 +4,26 @@ import { UpdateTrackerDto } from './dto/update-tracker.dto';
 import { Repository } from 'typeorm';
 import { Price } from './entities/price.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { MoralisService } from './moralis.service';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TrackerService {
 
   constructor(
     @InjectRepository(Price)
-    private readonly trackerRepository: Repository<Price>) {
+    private readonly trackerRepository: Repository<Price>,
+    private readonly moralisService: MoralisService
+  ) {
+  }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async handleCron() {
+    const ethPrice = await this.moralisService.getPrice('0x1');
+    const polyPrice = await this.moralisService.getPrice('0x89');
+
+    await this.create({chain: 'ethereum', price: ethPrice});
+    await this.create({chain: 'polygon', price: polyPrice});
   }
 
   async create(createTrackerDto: CreateTrackerDto) {
