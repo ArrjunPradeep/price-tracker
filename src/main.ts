e46c2a2b4126af5095d6ea2b2038ff832a733e66
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-10-15 15:42:31 +0530

    swagger-setup-configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Price Tracker API')
+    .setDescription('Track crypto prices and send alerts')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3369);
 }
 bootstrap();
