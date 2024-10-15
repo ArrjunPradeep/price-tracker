be98dfa59f71fe59c7842f43d64ad5b565202ad4
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-10-15 17:45:46 +0530

    service-updated

--- src/tracker/tracker.service.ts
+++ src/tracker/tracker.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTrackerDto } from './dto/create-tracker.dto';
 import { UpdateTrackerDto } from './dto/update-tracker.dto';
+import { Repository } from 'typeorm';
+import { Price } from './entities/price.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class TrackerService {
-  create(createTrackerDto: CreateTrackerDto) {
-    return 'This action adds a new tracker';
+
+  constructor(
+    @InjectRepository(Price)
+    private readonly trackerRepository: Repository<Price>) {
   }
 
-  findAll() {
-    return `This action returns all tracker`;
+  async create(createTrackerDto: CreateTrackerDto) {
+    const tracker = this.trackerRepository.create(createTrackerDto);
+    return await this.trackerRepository.save(tracker);
+  }
+
+  async findAll() {
+    return await this.trackerRepository.find();
   }
 
   findOne(id: number) {
