e450fd95d0129f7399e34d9b651f37fa08f84a9e
ArrjunPradeep <arjunpradeepm17@gmail.com>
2024-10-16 10:28:31 +0530

    node-mailer-configuration-added

--- src/tracker/tracker.module.ts
+++ src/tracker/tracker.module.ts
@@ -5,11 +5,34 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Price } from './entities/price.entity';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MoralisService } from './moralis.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MailerModule.forRootAsync({
+      imports: [
+        ConfigModule,
+      ],
+      useFactory: async(configService: ConfigService) => ({
+        transport: {
+          // Configuring the email transport host (SMTP server)
+          host: configService.get('EMAIL_HOST'),
+          // Configuring the email transport port
+          port: configService.get('EMAIL_PORT'),
+          // Disabling secure connection (not using SSL/TLS)
+          secure: false,
+          auth: {
+            // SMTP authentication: username for the email service
+            user: configService.get('EMAIL_USERNAME'),
+            // SMTP authentication: password for the email service
+            pass: configService.get('EMAIL_PASSWORD')
+          },
+        }
+      }),
+      inject: [ConfigService]
+    }),
     TypeOrmModule.forFeature([Price]),
     ScheduleModule.forRoot()
   ],
